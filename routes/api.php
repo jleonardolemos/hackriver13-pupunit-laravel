1dd285a42b491b3f33ad040a1e1eef9e4c33fa99
Leonardo Lemos <leonardo@mixd.com.br>
2018-04-13 13:41:08 -0300

    passa o retorno dos pokemons para a controller

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('pokemons', function () {
-    return \App\Pokemon::get();
-})->name('pokemons.index');
+Route::get('pokemons', 'PokemonsController@index')->name('pokemons.index');
